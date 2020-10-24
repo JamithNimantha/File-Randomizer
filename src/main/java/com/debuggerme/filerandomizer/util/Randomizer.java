b8cbc137af46cb3271a2553c4741aab07046c9f4
Jamith Nimantha <jamithnimantha@gmail.com>
2020-10-25 00:35:11 +0530

    REMOVED SOUTS

--- src/main/java/com/debuggerme/filerandomizer/util/Randomizer.java
+++ src/main/java/com/debuggerme/filerandomizer/util/Randomizer.java
@@ -8,20 +8,15 @@ import java.util.Arrays;
  * @author Jamith Nimantha
  */
 public class Randomizer {
-    private final MainController mainController;
 
     public Randomizer(MainController mainController) {
-        this.mainController = mainController;
     }
 
     public int[] generateSingleSet(int numbPerSet, int rangeBegin, int rangeEnd) {
         int [] numbersPerSet = new int [numbPerSet];
-//        int rangeBegin = 1;
-//        int rangeEnd = 20;
         boolean unique = true;
         for (int i = 0; i < numbersPerSet.length; i++) {
             numbersPerSet[i] = (int) ((Math.floor(Math.random() * ((rangeEnd + 1) - (rangeBegin)))) + (rangeBegin));
-//            System.out.println(numbersPerSet[i]);
             if (unique) {
                 for (int j = 0; j < i; j++) {
                     while (numbersPerSet[i] == numbersPerSet[j]){
@@ -32,16 +27,9 @@ public class Randomizer {
             }
         }
         Arrays.sort(numbersPerSet);
-        System.out.println(Arrays.toString(numbersPerSet));
         return numbersPerSet;
     }
 
-//    public static void main(String[] args) {
-//        int numberOfSets = 5;
-//        for (int i = 0; i < numberOfSets; i++) {
-//            generateSingleSet(3,1,20);
-//        }
-//    }
 
 
 }
